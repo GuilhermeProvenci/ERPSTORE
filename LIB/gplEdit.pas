6dd3d3a4fb4c93240297c3e50576f5535e7ad4ed
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-12-23 19:26:25 -0300

    LoadField Procedure

--- LIB/gplEdit.pas
+++ LIB/gplEdit.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
-  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, gplQry;
 
   type
   TgpEdit = class(TEdit)
@@ -13,10 +13,13 @@ uses
     FNumero: Real;
     FDataFieldName : string;
     FDataField: string;
+    FTable : string;
   public
     property Texto: string read FTexto write FTexto;
     property Numero: Real read FNumero write FNumero;
+    procedure LoadField(id : string);
   published
+    property Table : string read FTable write FTable;
     property DataFieldName: string Read FDataFieldName Write FDataFieldName;
     property DataField: string Read FDataField Write FDataField;
   end;
@@ -29,5 +32,28 @@ procedure Register;
 begin
   RegisterComponents('GPL', [TgpEdit]);
 end;
+
+{ TgpEdit }
+
+procedure TgpEdit.LoadField(id: string);
+var
+  SQL: string;
+  qry : TgpQry;
+begin
+  if FTable = '' then
+    Exit;
+
+  SQL := 'SELECT * FROM ' + FTable;
+  if id <> '0' then
+    SQL := SQL + ' WHERE id = ' + id
+  else
+    SQL := SQL + ' WHERE id is not null';
+
+  qry:= TgpQry.Create(self);
+  qry.SQLExec(SQL, []);
+
+  self.Text := qry.FieldByName(DataField).AsString;
+end;
+
 
 end.
