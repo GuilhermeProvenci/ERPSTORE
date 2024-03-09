ff86618e647dcb12f69189fb84d84ae51ec2986b
Guilherme Lima <guilhermelimamp2020@gmail.com>
2024-03-09 19:45:23 -0300

    Generatin Base Class X PrjUtils

---
+++ CLASSES/class_base.pas
@@ -0,0 +1,45 @@
+unit %s;
+
+interface
+
+uses
+  System.Classes, class_auxi;
+
+type
+  %s = class(%s)
+  private
+    FConf TAuxi;
+  public
+    constructor Create(AOwner TComponent); override;
+    destructor Destroy; override;
+  published
+    property Conf TAuxi read FConf write FConf;
+  end;
+
+procedure Register;
+
+implementation
+
+uses
+  Vcl.Dialogs, unit_funcoes;
+
+procedure Register;
+begin
+  RegisterComponents('GPL', [%s]);
+end;
+
+{ %s }
+
+constructor %s.Create(AOwner TComponent);
+begin
+  inherited Create(AOwner);
+  FConf = TAuxi.Create;
+end;
+
+destructor %s.Destroy;
+begin
+  FConf.Free;
+  inherited;
+end;
+
+end.
